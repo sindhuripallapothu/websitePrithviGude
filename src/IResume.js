f47d7f10fc675c58376ff0d7dd99b3d415eaf4e8
sindhuri pallapothu <lakshmisindhuri.pallapothu@copart.com>
2020-04-18 22:40:38 -0500

    April 18th

--- src/IResume.js
+++ src/IResume.js
@@ -4,7 +4,7 @@ const IResume = () => {
     return (
         <div id="Resume">
             <h1>INTERACTIVE RESUME</h1>
-            <iframe class= "ir" src="https://public.tableau.com/views/TableauResume_15843968725320/ResumeDashboard?:showVizHome=no&:embed=true"></iframe>
+            <iframe class= "ir" title="IResume" src="https://public.tableau.com/views/TableauResume_15843968725320/ResumeDashboard?:showVizHome=no&:embed=true"></iframe>
         </div>
     )
 }
